23986e99ae2b649e92249c5176b2efecb954eea6
Thural Musaibov <tural.musaibov@gmail.com>
2021-07-01 16:32:08 +0400

    added onclick js function

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'x16xel_tuned.png') {
+    if(mySrc === 'images/x16xel_tuned.png') {
       myImage.setAttribute('src','images/firefox2.png');
     } else {
-      myImage.setAttribute('src','x16xel_tuned.png');
+      myImage.setAttribute('src','images/x16xel_tuned.png');
     }
 }
