b23b8243093f357a66069cf92731e1addc690a56
Thural Musaibov <tural.musaibov@gmail.com>
2021-07-01 15:09:28 +0400

    add onclick pictures func js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/*const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'x16xel_tuned.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','x16xel_tuned.png');
+    }
+}
